da480a474d117d04084b2445fec074e6fa3c3a9a
Yessinta Dwi Rahayu <yessintadwi50@gmail.com>
2026-06-05 19:13:18 +0700

    Initial commit

---
+++ project_iot_led.ino
@@ -0,0 +1,39 @@
+int led_merah = 6;
+
+void setup() {
+  pinMode(led_merah, OUTPUT);
+
+
+
+  Serial.begin(19200);
+}
+
+void loop() {
+  if(Serial.available()>0) {
+    char perintah = Serial.read();
+
+    Serial.print("Menerima: ");
+    Serial.println(perintah);
+
+    // 1 = merah nyala
+    if (perintah == '1') {
+      digitalWrite(led_merah, HIGH);
+    }
+
+
+    // 4 = semua mati
+    else if (perintah == '4') {
+      digitalWrite(led_merah, LOW);
+
+
+    }
+
+    // 5 = semua nyala
+    else if (perintah == '5') {
+      digitalWrite(led_merah, HIGH);
+
+
+    }
+  }
+
+}
