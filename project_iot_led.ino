06b8b88b6ac27cb5fabc81261b52417f9f1b8b1d
Khansa Sahara <khansasahara22@gmail.com>
2026-06-06 15:30:57 +0700

    sudah mengerjakan bagian lampu kuning - khansa

--- project_iot_led.ino
+++ project_iot_led.ino
@@ -1,7 +1,9 @@
 int led_merah = 6;
+int led_kuning = 7
 
 void setup() {
   pinMode(led_merah, OUTPUT);
+  pinMode(led_kuning, OUTPUT);
 
 
 
@@ -20,18 +22,23 @@ void loop() {
       digitalWrite(led_merah, HIGH);
     }
 
+    // 2 = kuning nyala
+    if (perintah == '2') {
+      digitalWrite(led_kuning, HIGH);
+    }
+
 
     // 4 = semua mati
     else if (perintah == '4') {
       digitalWrite(led_merah, LOW);
-
+      digitalWrite(led_kuning, LOW);
 
     }
 
     // 5 = semua nyala
     else if (perintah == '5') {
       digitalWrite(led_merah, HIGH);
-
+      digitalWrite(led_kuning, HIGH);
 
     }
   }
