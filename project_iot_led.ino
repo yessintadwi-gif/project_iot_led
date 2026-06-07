4cbb8a7475b85e5a57a0f942b14cace17d9cfa46
Yessinta Dwi Rahayu <yessintadwi50@gmail.com>
2026-06-07 14:06:14 +0700

    sudah membuat fitur dashboard kontrol lampu

--- project_iot_led.ino
+++ project_iot_led.ino
@@ -1,9 +1,11 @@
 int led_merah = 6;
+int led_kuning = 7;
+int led_hijau= 8;
 
 void setup() {
   pinMode(led_merah, OUTPUT);
-
-
+  pinMode(led_kuning, OUTPUT);
+  pinMode(led_hijau, OUTPUT);
 
   Serial.begin(19200);
 }
@@ -20,19 +22,28 @@ void loop() {
       digitalWrite(led_merah, HIGH);
     }
 
+    // 2 = kuning nyala
+    else if (perintah == '2') {
+      digitalWrite(led_kuning, HIGH);
+    }
+
+    // 3 = hijau nyala
+    else if(perintah == '3') {
+      digitalWrite(led_hijau, HIGH);
+    }
 
     // 4 = semua mati
     else if (perintah == '4') {
       digitalWrite(led_merah, LOW);
-
-
+      digitalWrite(led_kuning, LOW);
+      digitalWrite(led_hijau, LOW);
     }
 
     // 5 = semua nyala
     else if (perintah == '5') {
       digitalWrite(led_merah, HIGH);
-
-
+      digitalWrite(led_kuning, HIGH);
+      digitalWrite(led_hijau, HIGH);
     }
   }
 
